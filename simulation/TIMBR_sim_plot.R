97abbf11e294408ef4a9a11ba1d37a92bc61381d
Wesley Crouse <wesleycrouse@gmail.com>
2020-09-25 15:12:23 -0400

    Adjusting simulation plots

--- simulation/TIMBR_sim_plot.R
+++ simulation/TIMBR_sim_plot.R
@@ -31,14 +31,16 @@ prior.uniform <- table(sapply(ewenss.calc(8, list(type="gamma", shape=1, rate=1)
 
 png(filename = "fig_3_0_prior.png", width = 480, height = 480)
 
-plot(1:8, prior.gamma, type="l", lty=5, col="grey", ylim=c(0,1), ylab="Prior Probability", xlab="Number of Alleles", main="Prior Distribution of Alleles")
-points(1:8, prior.gamma, pch=22, bg="grey")
-lines(1:8, prior.exp, lty=5, col="orange")
-points(1:8, prior.exp, pch=22, bg="orange")
-lines(1:8, prior.uniform, lty=5, col="darkcyan")
-points(1:8, prior.uniform, pch=22, bg="darkcyan")
+plot(1:8, prior.gamma, type="l", lty=5, col="grey", ylim=c(0,1), 
+     ylab="Prior Probability", xlab="Number of Alleles", main="Prior Distribution of Alleles",
+     cex.lab=1.5, cex.axis=1.2, cex.main=1.5, lwd=3)
+points(1:8, prior.gamma, pch=22, cex=1.5, bg="grey")
+lines(1:8, prior.exp, lty=5, lwd=3, col="orange")
+points(1:8, prior.exp, pch=22, cex=1.5, bg="orange")
+lines(1:8, prior.uniform, lty=5, lwd=3, col="darkcyan")
+points(1:8, prior.uniform, pch=22, cex=1.5, bg="darkcyan")
 
-legend(1, legend=c("Uniform", "Gamma", "Exponential"), col=c("darkcyan", "grey", "orange"), lty=5)
+legend(1, legend=c("Uniform", "Gamma", "Exponential"), col=c("darkcyan", "grey", "orange"), lty=5, lwd=3)
 
 dev.off()
 
@@ -55,7 +57,9 @@ par(mfrow = c(1, 2))
 
 for (v in c(0.1, 0.5)){
   for (A in c(1)){
-    plot(c(), c(), ylim=c(0,1), xlim=c(1,8), las=1, xlab="Number of Alleles", ylab = "0-1 Accuracy", frame.plot=T, cex.lab=1.5, cex.axis=1.3,
+    plot(c(), c(), ylim=c(0,1), xlim=c(1,8), 
+         las=1, cex.lab=1.5, cex.axis=1.2, cex.main=1.5, frame.plot=T,
+         xlab="Number of Alleles", ylab = "0-1 Accuracy",
          main=paste0("QTL Effect Size: ", v))
     
     for (h in (1:4)/5){
@@ -75,7 +79,8 @@ for (v in c(0.1, 0.5)){
   }
 }
 
-legend(1, 0.2, legend=c("Uniform", "Gamma", "Exponential"), col=c("darkcyan", "grey", "orange"), lty=1)
+legend(1, 0.2, legend=c("Uniform", "Gamma", "Exponential"), col=c("darkcyan", "grey", "orange"), 
+       lty=1, lwd=3, cex=1.2)
 
 dev.off()
 
@@ -92,7 +97,9 @@ par(mfrow = c(1, 2))
 
 for (v in c(0.1, 0.5)){
   for (A in c(1)){
-    plot(c(), c(), ylim=c(0,1), xlim=c(1,8), las=1, xlab="Number of Alleles", ylab = "Posterior Certainty", frame.plot=T, cex.lab=1.5, cex.axis=1.3,
+    plot(c(), c(), ylim=c(0,1), xlim=c(1,8), 
+         las=1, cex.lab=1.5, cex.axis=1.2, cex.main=1.5, frame.plot=T,
+         xlab="Number of Alleles", ylab = "Posterior Certainty", 
          main=paste0("QTL Effect Size: ", v))
     
     for (h in (1:4)/5){
@@ -110,7 +117,8 @@ for (v in c(0.1, 0.5)){
   }
 }
 
-legend(6, 1, legend=c("Uniform", "Gamma", "Exponential"), col=c("darkcyan", "grey", "orange"), lty=1)
+legend(5.5, 1, legend=c("Uniform", "Gamma", "Exponential"), col=c("darkcyan", "grey", "orange"), 
+       lty=1, lwd=3, cex=1.2)
 
 dev.off()
 
@@ -127,7 +135,9 @@ par(mfrow = c(1, 2))
 
 for (v in c(0.1, 0.5)){
   for (A in c(1)){
-    plot(c(), c(), ylim=c(1,8), xlim=c(1,8), las=1, xlab="Number of Alleles", ylab = "Expected Number of Alleles", frame.plot=T, cex.lab=1.5, cex.axis=1.3,
+    plot(c(), c(), ylim=c(1,8), xlim=c(1,8), 
+         las=1, cex.lab=1.5, cex.axis=1.2, cex.main=1.5, frame.plot=T,
+         xlab="Number of Alleles", ylab = "Expected Number of Alleles",
          main=paste0("QTL Effect Size: ", v))
     
     abline(0, 1, lty=3)
@@ -142,7 +152,8 @@ for (v in c(0.1, 0.5)){
   }
 }
 
-legend(5.5, 2.5, legend=c("Uniform", "Gamma", "Exponential"), col=c("darkcyan", "grey", "orange"), lty=1)
+legend(5.5, 2.5, legend=c("Uniform", "Gamma", "Exponential"), col=c("darkcyan", "grey", "orange"), 
+       lty=1, lwd=3, cex=1.2)
 
 dev.off()
 
@@ -162,7 +173,9 @@ par(mfrow = c(1, 2))
 
 for (v in c(0.1, 0.5)){
   for (A in c(1)){
-    plot(c(), c(), ylim=c(0,0.4), xlim=c(1, 8), las=1, xlab="Number of Alleles", ylab = "Mean Squared Error", frame.plot=T, cex.lab=1.5, cex.axis=1.3,
+    plot(c(), c(), ylim=c(0,0.4), xlim=c(1, 8), 
+         las=1, cex.lab=1.5, cex.axis=1.2, cex.main=1.5, frame.plot=T, 
+         xlab="Number of Alleles", ylab = "Mean Squared Error",
          main=paste0("QTL Effect Size: ", v))
     
     for (h in (1:4)/10){
@@ -179,7 +192,8 @@ for (v in c(0.1, 0.5)){
   }
 }
 
-legend(1, 0.4, legend=c("Uniform", "Gamma", "Exponential", "Full", "Known"), col=c("darkcyan", "grey", "orange", "black", "green"), lty=1)
+legend(1, 0.4, legend=c("Uniform", "Gamma", "Exponential", "Full", "Known"), 
+       col=c("darkcyan", "grey", "orange", "black", "green"), lty=1, lwd=3, cex=1.2)
 
 dev.off()
 
@@ -198,7 +212,9 @@ par(mfrow = c(1, 2))
 
 v <- 0.1
 
-plot(c(), c(), ylim=c(0,1), xlim=c(1,8), las=1, xlab="Number of Alleles", ylab = "0-1 Accuracy", frame.plot=T, cex.lab=1.5, cex.axis=1.3,
+plot(c(), c(), ylim=c(0,1), xlim=c(1,8), 
+     las=1, cex.lab=1.5, cex.axis=1.2, cex.main=1.5, frame.plot=T,
+     xlab="Number of Alleles", ylab = "0-1 Accuracy",
      main=paste0("QTL Effect Size: ", v))
 
 for (h in (1:4)/5){
@@ -215,11 +231,15 @@ for (i in 1:length(strategies)){
   points(data.subset$alleles, data.subset[,2], col=colors[i], pch=16)
 }
 
-legend(5.5, 1, legend=c("CRP", "Tree", "Misspecified", "Incorrect"), col=c("orange", "blue", "purple", "red"), lty=1)
+legend(5.5, 1, legend=c("CRP", "Tree", "Misspecified", "Incorrect"), 
+       col=c("orange", "blue", "purple", "red"),
+       lty=1, lwd=3, cex=1.2)
 
 v <- 0.5
 
-plot(c(), c(), ylim=c(0,1), xlim=c(1,8), las=1, xlab="Number of Alleles", ylab = "0-1 Accuracy", frame.plot=T, cex.lab=1.5, cex.axis=1.3,
+plot(c(), c(), ylim=c(0,1), xlim=c(1,8), 
+     las=1, cex.lab=1.5, cex.axis=1.2, cex.main=1.5, frame.plot=T,
+     xlab="Number of Alleles", ylab = "0-1 Accuracy",
      main=paste0("QTL Effect Size: ", v))
 
 for (h in (1:4)/5){
@@ -245,13 +265,15 @@ accuracy <- aggregate(posterior.M ~ strategy + alleles + alpha + var.exp, data=r
 accuracy <- cbind(accuracy[,1:4], accuracy$posterior.M)
 colnames(accuracy)[-c(1:4)] <- c("posterior.M", "lower", "upper")
 
-for (A in c(0.1, 1, 10)){
+for (A in c(1)){
   png(filename = paste0("fig_3_2_e_", A, "_posterior_M.png"), width = 960, height = 480)
   
   par(mfrow = c(1, 2))
   
   for (v in c(0.1, 0.5)){
-    plot(c(), c(), ylim=c(0,1), xlim=c(1,8), las=1, xlab="Number of Alleles", ylab = "Posterior Certainty", frame.plot=T, cex.lab=1.5, cex.axis=1.3,
+    plot(c(), c(), ylim=c(0,1), xlim=c(1,8), 
+         las=1, cex.lab=1.5, cex.axis=1.2, cex.main=1.5, frame.plot=T,
+         xlab="Number of Alleles", ylab = "Posterior Certainty",
          main=paste0("QTL Effect Size: ", v))
     
     for (h in (1:4)/5){
@@ -268,10 +290,11 @@ for (A in c(0.1, 1, 10)){
     }
   }
   
-  legend(1, 0.2, legend=c("CRP", "Tree", "Misspecified", "Incorrect"), col=c("orange", "blue", "purple", "red"), lty=1)
+  legend(1, 0.3, legend=c("CRP", "Tree", "Misspecified", "Incorrect"), 
+         col=c("orange", "blue", "purple", "red"), 
+         lty=1, lwd=3, cex=1.2)
   
   dev.off()
-  
 }
 
 ####################
@@ -291,7 +314,9 @@ png(filename = paste0("fig_3_5_e_", A, "_MSE.png"), width = 960, height = 480)
 par(mfrow = c(1, 2))
 
 for (v in c(0.1, 0.5)){
-  plot(c(), c(), ylim=c(0,0.4), xlim=c(1, 8), las=1, xlab="Number of Alleles", ylab = "Mean Squared Error", frame.plot=T, cex.lab=1.5, cex.axis=1.3,
+  plot(c(), c(), ylim=c(0,0.4), xlim=c(1, 8), 
+       las=1, cex.lab=1.5, cex.axis=1.2, cex.main=1.5, frame.plot=T,
+       xlab="Number of Alleles", ylab = "Mean Squared Error",
        main=paste0("QTL Effect Size: ", v))
   
   for (h in (1:4)/10){
@@ -307,6 +332,8 @@ for (v in c(0.1, 0.5)){
   }
 }
 
-legend(1, 0.4, legend=c("CRP", "Tree", "Misspecified", "Incorrect", "Full", "Known"), col=c("orange", "blue", "purple", "red", "black", "green"), lty=1)
+legend(1, 0.4, legend=c("CRP", "Tree", "Misspecified", "Incorrect", "Full", "Known"), 
+       col=c("orange", "blue", "purple", "red", "black", "green"), 
+       lty=1, lwd=3, cex=1.2)
 
 dev.off()
