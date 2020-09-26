82c0c16bbcdf3e2fbd103f9a161e5cab0e039985
Wesley Crouse <wesleycrouse@gmail.com>
2020-09-26 09:17:26 -0400

    Update install_TIMBR.R

--- install_TIMBR.R
+++ install_TIMBR.R
@@ -1,5 +1,5 @@
-#install frozen version of TIMBR included with submission
-install.packages('TIMBR-master.zip', repos = NULL)
+#install frozen version of TIMBR included with manuscript
+#install.packages('TIMBR-master.zip', repos = NULL)
 
 #install latest version of TIMBR from GitHub
-#devtools::install_github("wesleycrouse/TIMBR")
+devtools::install_github("wesleycrouse/TIMBR")
